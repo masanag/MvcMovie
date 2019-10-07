807b5d4bddcde1ea1f588e77a45eff868f778421
masanag <masaki.nagaishi@gmail.com>
2019-10-08 08:14:15 +0900

    Add View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,17 +8,19 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int id =1)
+        public IActionResult Welcome(string name, int numTimes =1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {id}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
